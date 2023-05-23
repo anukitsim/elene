addfe4873a06f0105e7ef8819bde8ec948aca7e6
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-05-23 16:52:49 +0200

    built add to cart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,16 +1,26 @@
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
 
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
 const ProductCard = ({ product }) => {
-    const {name, price, imageUrl} = product;
+    const {name, price, imageUrl, currency} = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
+
   return(
     <div className="product-card-container">
+        
         <img src={imageUrl} alt={`${name}`}/>
         <div className="product-footer">
+        <span className="name">{name}</span>
             <span className="price">{price}</span>
+            <span className="currency">{currency}</span>
            
         </div>
-        <Button>Add to cart</Button>
+        <Button onClick={addProductToCart}>Add to cart</Button>
     </div>
   )
 };
